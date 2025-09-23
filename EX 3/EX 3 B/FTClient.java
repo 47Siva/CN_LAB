2b7e1d81db6f8d61ca5efe3bc2e0f45d0a1843d6
47Siva <sivabaskaran2003@gmail.com>
2025-09-23 15:59:00 +0530

    change EX 3 B

--- EX 3/EX 3 B/FTClient.java
+++ EX 3/EX 3 B/FTClient.java
@@ -2,28 +2,27 @@ import java.io.*;
 import java.net.*;
 
 public class FTClient {
-    public static void main(String[] args) throws IOException {
-        try {
-            Socket socket = new Socket(InetAddress.getLocalHost(), 4000);
+    public static void main(String[] args) {
+        final int PORT = 4000;
+
+        try (Socket socket = new Socket(InetAddress.getLocalHost(), PORT)) {
             System.out.println("Connected to server.");
 
-            // Receive file from server
-            byte[] bytearray = new byte[1024 * 1024]; // 1MB buffer
-            InputStream is = socket.getInputStream();
+            // Save received file as "received.txt"
+            try (InputStream is = socket.getInputStream();
+                 FileOutputStream fos = new FileOutputStream("received.txt");
+                 BufferedOutputStream bos = new BufferedOutputStream(fos)) {
 
-            FileOutputStream fos = new FileOutputStream("received.txt");
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-            int bytesRead;
-            while ((bytesRead = is.read(bytearray, 0, bytearray.length)) != -1) {
-                bos.write(bytearray, 0, bytesRead);
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = is.read(buffer)) != -1) {
+                    bos.write(buffer, 0, bytesRead);
+                }
+                System.out.println("File received successfully as received.txt");
             }
 
-            bos.close();
-            socket.close();
-            System.out.println("File received successfully.");
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Client error: " + e.getMessage());
         }
     }
 }
--- EX 3/EX 3 B/FTServer.java
+++ EX 3/EX 3 B/FTServer.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.net.*;
 
 public class FTServer {
-    public static void main(String[] args) throws IOException {
-        try {
-            // Start server on port 4000
-            ServerSocket serverSocket = new ServerSocket(4000);
+    public static void main(String[] args) {
+        final int PORT = 4000;
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started. Waiting for connection...");
 
             Socket socket = serverSocket.accept();
@@ -13,24 +13,29 @@ public class FTServer {
 
             // File to send
             File transferFile = new File("send.txt");
+            if (!transferFile.exists()) {
+                System.out.println("Error: File not found -> " + transferFile.getAbsolutePath());
+                socket.close();
+                return;
+            }
+
             byte[] bytearray = new byte[(int) transferFile.length()];
 
-            FileInputStream fin = new FileInputStream(transferFile);
-            BufferedInputStream bin = new BufferedInputStream(fin);
-            bin.read(bytearray, 0, bytearray.length);
+            try (FileInputStream fin = new FileInputStream(transferFile);
+                 BufferedInputStream bin = new BufferedInputStream(fin);
+                 OutputStream os = socket.getOutputStream()) {
 
-            // Send file to client
-            OutputStream os = socket.getOutputStream();
-            System.out.println("Sending file...");
-            os.write(bytearray, 0, bytearray.length);
-            os.flush();
+                bin.read(bytearray, 0, bytearray.length);
+                System.out.println("Sending file...");
 
-            System.out.println("File transfer complete");
+                os.write(bytearray, 0, bytearray.length);
+                os.flush();
+                System.out.println("File transfer complete.");
+            }
 
             socket.close();
-            serverSocket.close();
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Server error: " + e.getMessage());
         }
     }
 }
